a4d27b43c8d7d28e7cfcf2c469b5ad17c46e577c
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2024-10-14 16:36:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 function getComputerChoice(){
     const choices =['rock', 'paper', 'scissors'];
     const randomChoice= Math.floor(Math.random() * choices.length);
-    console.log(choices[randomChoice]);
+    return choices[randomChoice];
+
 
 }
- getComputerChoice();
 
-function getPlayerChoice(){
-    const userChoice = window.prompt('Enter rock, paper or scissors').trim().toLowerCase();
-    if (!["rock", "paper", "scissors"].includes(userChoice)) {
-        window.alert("please choose rock or paper or scissors")
+function determineWinner(playerChoice, computerChoice){
+    
+   if (playerChoice === computerChoice){
+    return ("It's a tie!");
+   }
+   else if ((playerChoice=== "rock" && computerChoice === "scissors") ||
+            (playerChoice=== "scissors" && computerChoice === "paper") ||
+           (playerChoice=== "paper" && computerChoice === "rock")){
+            return "Player wins"
+           }
+           else{
+            return("computer wins");
+           }
+
+}
+
+function playGame(){
+    const playerChoice = window.prompt('Enter rock, paper or scissors').trim().toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    if (!["rock", "paper", "scissors"].includes(playerChoice)) {
+        window.alert("please choose rock or paper or scissors");
     }
-    else{
-        console.log(userChoice)
-    };
+    else {
+        const result = determineWinner(playerChoice, computerChoice);
+        window.alert(`you chose ${playerChoice}/ the engine chose ${computerChoice}/ result is ${result}`)
+    }
 }
-getPlayerChoice()
+
+playGame();
